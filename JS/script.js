918359e05f7515971ead4cde516a57651fd67421
Madyca24 <mads@cajar.dk>
2025-03-06 11:46:58 +0100

    this -> user1 fix comment

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,19 @@
-class user {
+class User {
     constructor(name, username, password) {
         this.name = name;
         this.username = username;
         this.password = password;
     }
 }
-const user1 = new user('Mads', 'mads1234', '1234')
+const user1 = new User('Mads', 'mads1234', '1234')
 
 function checkLogin() {
     const usern = document.querySelector("#uname");
     const paword = document.querySelector("#pword");
     let index = 0;
-    user.allInstances = [];
-    user.allInstances.push(this);
-    let userList = user.allInstances
+    User.allInstances = [];
+    User.allInstances.push(user1);
+    let userList = User.allInstances
     console.log(userList.length);
     for(let i = 0; i < userList.length; i++) {
         if (userList[i].username === usern.value){
@@ -26,7 +26,6 @@ function checkLogin() {
         console.log(userList[index].name)
         document.getElementById("welcome").innerHTML = "" + userList[index].name
     }
-
 }
 
 
